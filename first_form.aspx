d0f91647a1647c12cafa92f859f7c4d466dbc2e7
sameenshafeeq <sameenshafeeq123@gmail.com>
2022-07-25 18:27:29 -0800

    Add files via upload

---
+++ first_form.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="first_form.aspx.cs" Inherits="task01_view_state.first_form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--<asp:textbox id="TextBox1" runat="server" TextMode="Number"></asp:textbox>  <br />--%>
+            <input  id ="my_input_data" type="number" runat="server" value="0" />
+            <br />
+            <asp:Button ID="my_button" runat="server" Text="Click here to increment the value!" OnClick="my_button_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ first_form.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace task01_view_state
+{
+    public partial class first_form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ViewState["my_input_data1"] = my_input_data.Value;
+            ////my_input_data.Value = 0;
+        }
+
+        protected void my_button_Click(object sender, EventArgs e)
+        {
+
+            ViewState["my_input_data1"]
+
+
+                int num = ViewState["my_input_data1"] + 1;
+            my_input_data = ViewState["my_input_data1"];
+        }
+    }
+}
